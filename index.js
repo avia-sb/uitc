8762f81a59f4027c981db9ea53e3f891b0e460e6
AviaG <avia.godis@sparkbeyond.com>
2020-02-26 17:19:47 +0200

    added login function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-export function login(username, password, url){
-    cy.visit(url)
-    cy.get('#username').type(username)
-    cy.get('#password').type(password)
-    cy.get('[name="login"]').click()
+declare module 'uitc'{
+  export function login(username, password, url){
+      cy.visit(url)
+      cy.get('#username').type(username)
+      cy.get('#password').type(password)
+      cy.get('[name="login"]').click()
+  }
 }
 
