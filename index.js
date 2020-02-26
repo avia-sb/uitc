ed1e8ed5eba6ce97621517ec8b230243b8e8f4a9
AviaG <avia.godis@sparkbeyond.com>
2020-02-26 17:10:15 +0200

    added login function

---
+++ index.js
@@ -0,0 +1,7 @@
+function login(username, password, url){
+    cy.visit(url)
+    cy.get('#username').type(username)
+    cy.get('#password').type(password)
+    cy.get('[name="login"]').click()
+}
+
